97ad34aa2776e85a058f5c19e45579ee8b3afbe5
Bankole Samuel <109220782+Classiq04@users.noreply.github.com>
2025-08-12 12:20:07 -0400

    Add files via upload

---
+++ routetable.tf
@@ -0,0 +1,13 @@
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  
+  tags = {
+    Name = "Route Table"
+  }
+}
